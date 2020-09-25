5b247ae8bfbcfecb6abbdce46780fe6549c6d5dc
Leo-BG <thaison1815@gmail.com>
2020-09-25 16:18:01 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import ColorBox from './components/ColorBox';
 
 function App() {
   return (
     <div className="app">
       <h1>React Hook</h1>
+      <ColorBox />
     </div>
   );
 }
